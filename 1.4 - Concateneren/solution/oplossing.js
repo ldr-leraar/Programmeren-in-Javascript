58efd3f0e5255edfcedce11af7d15d58571507a7
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-19 10:27:40 +0200

    Add files via upload

---
+++ 1.4 - Concateneren/solution/oplossing.js
@@ -0,0 +1,6 @@
+var voornaam = 'Helmut'; 
+var familienaam = 'Lotti'; 
+var geboorteplaats = 'Gent'; 
+var adres = 'Dorpstraat 13, Sint-Amandsberg'; 
+
+console.log(voornaam + ' ' + familienaam + ', geboren in ' + geboorteplaats + ', woont in ' + adres + '.');
