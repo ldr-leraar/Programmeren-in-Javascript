43f152dedef834e288abd27d8ad752b2f95cd98a
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-19 10:59:43 +0200

    Add files via upload

---
+++ 2.10 - Bytes/solution/oplossing.js
@@ -0,0 +1,12 @@
+var bestandInGigabytes = 6.66;
+
+var bestandInMegabytes = bestandInGigabytes * 1024;
+var bestandInKilobytes = bestandInMegabytes * 1024;
+var bestandInBytes = bestandInKilobytes * 1024;
+var bestandInBits = bestandInBytes * 8;
+
+console.log(bestandInGigabytes + ' gigabytes');
+console.log(bestandInMegabytes + ' megabytes');
+console.log(bestandInKilobytes + ' kilobytes');
+console.log(bestandInBytes + ' bytes');
+console.log(bestandInBits + ' bits');
