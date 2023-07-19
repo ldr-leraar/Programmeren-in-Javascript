d02e8c9a522e7e6c7b5d9b66f3b7b34979922ecc
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-19 10:33:37 +0200

    Add files via upload

---
+++ 1.5 - Goedemorgen Robot/solution/oplossing.js
@@ -0,0 +1,11 @@
+var begroeting = 'Goedemorgen';
+var titel1 = 'mevr.';
+var naam1 = 'Deleye';
+var activiteit1 = 'vergadering met regionaal manager Sufyan Y.';
+
+var titel2 = 'mr.';
+var naam2 = 'Lantmeeters';
+var activiteit2 = 'beklimming Belfort met Erasmus+groep uit Portugal.';
+
+console.log(begroeting + ' ' + titel1 + ' ' + naam1 + ', vandaag heeft u op de planning: ' + activiteit1);
+console.log(begroeting + ' ' + titel2 + ' ' + naam2 + ', vandaag heeft u op de planning: ' + activiteit2);
