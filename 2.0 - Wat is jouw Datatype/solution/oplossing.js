003710cfb99bb861b95caf68f8929232604c903b
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-19 10:41:30 +0200

    Add files via upload

--- 2.0 - Wat is jouw Datatype/solution/oplossing.js
+++ 2.0 - Wat is jouw Datatype/solution/oplossing.js
@@ -1,11 +1,4 @@
-var begroeting = 'Goedemorgen';
-var titel1 = 'mevr.';
-var naam1 = 'Deleye';
-var activiteit1 = 'vergadering met regionaal manager Sufyan Y.';
-
-var titel2 = 'mr.';
-var naam2 = 'Lantmeeters';
-var activiteit2 = 'beklimming Belfort met Erasmus+groep uit Portugal.';
-
-console.log(begroeting + ' ' + titel1 + ' ' + naam1 + ', vandaag heeft u op de planning: ' + activiteit1);
-console.log(begroeting + ' ' + titel2 + ' ' + naam2 + ', vandaag heeft u op de planning: ' + activiteit2);
+var rekening = 500; 
+var storting = 275; 
+rekening = rekening + storting;
+console.log('Uw nieuw saldo bedraagt ' + rekening + ' euro.');
