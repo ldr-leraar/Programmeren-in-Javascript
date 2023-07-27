7ef11e5104f92666b280d94c2dd60f225574e669
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-27 14:49:42 +0200

    Add files via upload

---
+++ 2.6 - Gemiddelde Autorit/solution/oplossing.js
@@ -0,0 +1,15 @@
+var gentLille = 74.2;
+var lilleParijs = 255;
+var parijsLeHavre = 260;
+var leHavreLeMans = 211;
+var leMansLimoges = 336;
+var limogesNarbonne = 439;
+var narbonneJuanLesPins = 400;
+
+var totaalAfgelegdeKm = gentLille + lilleParijs + parijsLeHavre + leHavreLeMans + leMansLimoges + limogesNarbonne + narbonneJuanLesPins;
+
+var aantalRitten = 7;
+
+var gemiddeldeRit = totaalAfgelegdeKm / aantalRitten;
+
+console.log("De gemiddelde rit die Fred gemaakt heeft is " + gemiddeldeRit + " kilometer lang.");
