c3ab42da395e6b4739396c9235e651b63adfda95
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-27 14:52:49 +0200

    Add files via upload

---
+++ 2.7 - Huurwaarborg/solution/oplossing.js
@@ -0,0 +1,7 @@
+var maandhuur = 695;
+var waarborg = maandhuur * 3;
+var interestpercentage = 19.87 / 100;
+
+var totaalNaVijfJaar = waarborg + (waarborg * interestpercentage);
+
+console.log("Na vijf jaar krijgt Fred " + totaalNaVijfJaar + " euro terug van zijn waarborg.");
