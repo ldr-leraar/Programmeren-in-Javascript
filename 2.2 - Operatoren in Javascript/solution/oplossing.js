52ef49ddfccd76f33149602ee988f5d82a9b37d0
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-27 13:26:47 +0200

    Add files via upload

---
+++ 2.2 - Operatoren in Javascript/solution/oplossing.js
@@ -0,0 +1,15 @@
+var x = 10;
+var y = 5;
+var z = 4;
+
+console.log(x + y); // som maken
+
+console.log(x - z); // verschil maken
+
+console.log(x * y); // vermenigvuldiging uitvoeren
+
+console.log(x / z); // delen
+
+console.log(x ** y); // exponentiële berekening
+
+console.log(x % z); // berekening van de rest na deling
