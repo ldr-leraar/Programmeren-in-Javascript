a1bcedffbbcb50d5c51823ad6b8d0c2a10a59851
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-28 13:52:57 +0200

    Add files via upload

---
+++ 3.4 - Aanmelden/solution/oplossing.js
@@ -0,0 +1,11 @@
+// Definieer het correcte wachtwoord
+var correctWachtwoord = '@B0nJ0uR!';
+
+// Vraag het wachtwoord van de gebruiker
+var gebruikerWachtwoord = '@BOnJOuR!'
+// Controleer of het wachtwoord correct is
+if (gebruikerWachtwoord == correctWachtwoord) {
+    console.log("Verbinding gelukt");
+} else {
+    console.log("Verbinding mislukt");
+}
