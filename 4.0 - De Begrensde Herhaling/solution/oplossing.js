2c7ec3267b47f498cf73ff638f8ef966ddb06dbc
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-07-29 15:57:34 +0200

    Add files via upload

---
+++ 4.0 - De Begrensde Herhaling/solution/oplossing.js
@@ -0,0 +1,8 @@
+// Start de countdown bij 10
+for (var teller = 10; teller > 0; teller--) {
+    // Print het huidige nummer van de countdown naar het scherm
+    console.log(teller);
+}
+
+// Print "LANCERING" naar het scherm wanneer de countdown is afgelopen
+console.log("LANCERING");
