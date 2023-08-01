851c419c22afc7f4fecb74cbf7febb101ebaf2c2
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-01 12:47:18 +0200

    Add files via upload

---
+++ 6.2 - Sequentie Opdracht 2/solution/oplossing.js
@@ -0,0 +1,8 @@
+var straal = prompt('Voer de straal van de cirkel in:');
+var pi = 3.14;
+
+var omtrek = 2 * pi * straal;
+var oppervlakte = pi * straal * straal;
+
+console.log("De omtrek van de cirkel is " + omtrek + " meter.");
+console.log("De oppervlakte van de cirkel is " + oppervlakte + " vierkante meter.");
