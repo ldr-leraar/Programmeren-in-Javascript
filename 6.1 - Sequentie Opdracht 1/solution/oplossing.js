ed90ac45932d844260acf462f205f635287e4ffa
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-08-01 12:41:22 +0200

    Add files via upload

---
+++ 6.1 - Sequentie Opdracht 1/solution/oplossing.js
@@ -0,0 +1,8 @@
+var lengte = prompt('Voer de lengte van het veld in:');
+var breedte = prompt('Voer de breedte van het veld in:');
+
+var omtrek = 2 * (lengte + breedte);
+var oppervlakte = lengte * breedte;
+
+console.log("De omtrek van het veld is " + omtrek+ " meter.");
+console.log("De oppervlakte van het veld is " + oppervlakte + " vierkante meter.");
