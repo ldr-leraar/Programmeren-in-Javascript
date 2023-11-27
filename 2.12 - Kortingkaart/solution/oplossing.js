1b250cc0c24de72860cbc88d95c5a2b9ec8056bd
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2023-11-27 22:11:21 +0100

    Update oplossing.js

--- 2.12 - Kortingkaart/solution/oplossing.js
+++ 2.12 - Kortingkaart/solution/oplossing.js
@@ -3,7 +3,7 @@ var bedrag1 = 102.77;
 
 // Berekening van de korting en het totale bedrag
 var korting = 100 * 0.05
-var totaalTeBetalen = bedrag1 - korting + 2.77;
+var totaalTeBetalen = 100 - korting + 2.77;
 
 // Afdrukken van het resultaat
 console.log('Het totale te betalen bedrag is', totaalTeBetalen, 'euro.');
